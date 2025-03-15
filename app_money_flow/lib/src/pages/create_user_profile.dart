fc4316b200eedc835535afcf866ddbfb32b2253f
PedroBarion <pepebarion@gmail.com>
2025-03-15 16:22:16 -0300

    Create_user_profile1

--- app_money_flow/lib/src/pages/create_user_profile.dart
+++ app_money_flow/lib/src/pages/create_user_profile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart'; // Adicione esta linha
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,38 +8,40 @@ class CreateUserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: PreferredSize(
-    preferredSize: const Size.fromHeight(80), // Define a altura da AppBar
-    child: AppBar(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      elevation: 4,
-      shadowColor: Colors.black12,
-      centerTitle: true,
-      title: Column(
-        children: [
-          const SizedBox(height: 30), // Adiciona espaçamento no topo
-          Row(
-            mainAxisSize: MainAxisSize.min,
+      backgroundColor: Color(0xFFFFFFFF),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          centerTitle: true,
+          title: Column(
             children: [
-              SvgPicture.asset(
-                'assets/icons/Carteira.svg',
-                height: 30,
-                colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
-              ),
-              const SizedBox(width: 8),
-              const Text(
-                'MoneyFlow',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
-                ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/Carteira.svg',
+                    height: 25,
+                    colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'MoneyFlow',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),),),
-
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
         child: Column(
@@ -62,26 +65,60 @@ class CreateUserProfile extends StatelessWidget {
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
-                color: Color(0xFF087F5B), 
+                color: Color(0xFF087F5B),
               ),
             ),
             const SizedBox(height: 20),
             SvgPicture.asset('assets/images/Questions.svg', height: 150),
             const SizedBox(height: 20),
-            const Text(
-              'Ao continuar, estou de acordo com os Termos de Uso e com o Aviso de Privacidade do MoneyFlow.',
+            RichText(
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12, color: Colors.black54),
+              text: TextSpan(
+                style: const TextStyle(fontSize: 12, color: Colors.black54),
+                children: [
+                  const TextSpan(text: 'Ao continuar, estou de acordo com os '),
+                  TextSpan(
+                    text: 'Termos de Uso',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF087F5B),
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        print("Clicou nos Termos de Uso");
+                      },
+                  ),
+                  const TextSpan(text: ' e com o '),
+                  TextSpan(
+                    text: 'Aviso de Privacidade',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF087F5B),
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        print("Clicou no Aviso de Privacidade");
+                      },
+                  ),
+                  const TextSpan(text: ' do MoneyFlow.'),
+                ],
+              ),
             ),
             const SizedBox(height: 16),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-                  // Navegação para próxima tela
+                  // Navegação para a segunda tela (CreateUserProfile)
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const CreateUserProfile()),
+                  );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF087F5B), 
+                  backgroundColor: const Color(0xFF087F5B),
                   padding: const EdgeInsets.symmetric(vertical: 16),
                 ),
                 child: const Text(
