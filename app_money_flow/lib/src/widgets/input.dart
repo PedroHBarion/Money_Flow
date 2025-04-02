94bcfd32badd7455dd2ab26664103f7e187840ce
anamagalhaesss <ana.magalhaes@sou.unifeob.edu.br>
2025-04-01 22:01:39 -0300

    feat:  implemetns obscure Icon to input
    Edição no botão de visualização de senha, para ficar mais harmonico.

--- app_money_flow/lib/src/widgets/input.dart
+++ app_money_flow/lib/src/widgets/input.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
 
-class Input extends StatelessWidget {
+class Input extends StatefulWidget {
   final String label;
   final TextEditingController controller;
   final String? errorText;
+  final bool obscureText; // Propriedade para indicar se o campo é de senha
 
   const Input({
-    super.key,
+    Key? key,
     required this.label,
     required this.controller,
     this.errorText,
-  });
+    this.obscureText = false, // Padrão é não obscurecer
+  }) : super(key: key);
+
+  @override
+  _InputState createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  late bool _obscure; // Estado para controlar a visibilidade da senha
+
+  @override
+  void initState() {
+    super.initState();
+    _obscure = widget.obscureText; // Inicializa com o valor da propriedade
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
+      controller: widget.controller,
+      obscureText: _obscure, // Controla a visibilidade do texto
       decoration: InputDecoration(
-        labelText: label,
+        labelText: widget.label,
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        errorText: errorText, // Exibe o erro se houver
+        errorText: widget.errorText,
+        suffixIcon:
+            widget.obscureText
+                ? IconButton(
+                  icon: Icon(
+                    _obscure ? Icons.visibility_off : Icons.visibility,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscure = !_obscure; // Alterna a visibilidade
+                    });
+                  },
+                )
+                : null, // Exibe o ícone apenas se for senha
       ),
     );
   }
