1b7e21266114a79b71bc31dab7d2388f52e58abe
anamagalhaesss <ana.magalhaes@sou.unifeob.edu.br>
2025-04-01 22:08:32 -0300

    refactor: update login Model

--- app_money_flow/lib/src/core/models/login_model.dart
+++ app_money_flow/lib/src/core/models/login_model.dart
@@ -1,19 +1,12 @@
-class LoginModel {
-  final String email;
-  final String password;
+import 'dart:convert';
 
+class LoginModel {
+  String email;
+  String password;
 
   LoginModel({required this.email, required this.password});
 
-
-  bool isValidEmail() {
-    final emailRegex = RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}");
-    return emailRegex.hasMatch(email);
-  }
-
-
-  bool isValidPassword() {
-    final passwordRegex = RegExp(r"^\d{8}$");
-    return passwordRegex.hasMatch(password);
+  String toJson() {
+    return jsonEncode({'email': email, 'password': password});
   }
 }
