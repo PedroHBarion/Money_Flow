65a542dc8a31c5ca817d17a79f5c9a182c4d076e
Bruno <brunopppinheiro@gmail.com>
2025-04-01 22:18:12 -0300

    Refactor: junção do Month_Navigation

--- app_money_flow/lib/src/widgets/transactions_container.dart
+++ app_money_flow/lib/src/widgets/transactions_container.dart
@@ -1,6 +1,6 @@
 import 'package:app_money_flow/src/widgets/dropdown_transaction.dart';
 import 'package:app_money_flow/src/widgets/filter_modal.dart';
-import 'package:app_money_flow/src/widgets/month_navigation.dart';
+import 'package:app_money_flow/src/widgets/custom_month_navigation.dart';
 import 'package:app_money_flow/src/widgets/transaction_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -17,20 +17,6 @@ class _TransactionsContainerState extends State<TransactionsContainer> {
   int? selectedYear;
 
   int currentMonthIndex = 0; // Exemplo de índice de mês selecionado
-  List<String> months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
 
   void changeMonth(int direction) {
     setState(() {
@@ -55,85 +41,83 @@ class _TransactionsContainerState extends State<TransactionsContainer> {
     );
   }
 
-  int touchedIndex = -1; // Índice de seção tocada
-
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-        decoration: BoxDecoration(
-          color: Color(0xFFF1F3F5),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                DropdownTransaction(),
-                IconButton(
-                  onPressed: _openFilterModal,
-                  icon: SvgPicture.asset('assets/icons/filter_icon.svg'),
-                )
-              ],
-            ),
-            SizedBox(height: 20),
-            MonthNavigation(
-              currentMonthIndex: currentMonthIndex,
-              months: months,
-              onMonthChanged: changeMonth,
-            ),
-            SizedBox(height: 10),
-            
-               Column(
-                children: [
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "02/20/06",
-                    amount: "-R\$ 693,00",
-                  ),
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "Roupas",
-                    amount: "-R\$ 693,00",
-                  ),
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "Roupas",
-                    amount: "-R\$ 693,00",
-                  ),
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "02/20/06",
-                    amount: "-R\$ 693,00",
-                  ),
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "Roupas",
-                    amount: "-R\$ 693,00",
-                  ),
-                  TransactionCard(
-                    color: 0xFFEBFBEE,
-                    iconPath: 'assets/icons/transaction_icon.svg',
-                    title: "Almoço",
-                    date: "Roupas",
-                    amount: "-R\$ 693,00",
-                  ),
-                ],
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      decoration: BoxDecoration(
+        color: Color(0xFFF1F3F5),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              DropdownTransaction(),
+              IconButton(
+                onPressed: _openFilterModal,
+                icon: SvgPicture.asset('assets/icons/filter_icon.svg'),
               ),
-            
-          ],
-        ));
+            ],
+          ),
+          SizedBox(height: 20),
+          CustomMonthNavigation(
+            currentMonthIndex: currentMonthIndex,
+            onMonthChanged: changeMonth,
+          ),
+
+          SizedBox(height: 10),
+
+          Column(
+            children: [
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "02/20/06",
+                amount: "-R\$ 693,00",
+              ),
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "Roupas",
+                amount: "-R\$ 693,00",
+              ),
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "Roupas",
+                amount: "-R\$ 693,00",
+              ),
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "02/20/06",
+                amount: "-R\$ 693,00",
+              ),
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "Roupas",
+                amount: "-R\$ 693,00",
+              ),
+              TransactionCard(
+                color: 0xFFEBFBEE,
+                iconPath: 'assets/icons/transaction_icon.svg',
+                title: "Almoço",
+                date: "Roupas",
+                amount: "-R\$ 693,00",
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
