16e956163043f5991d3f6144157c825cdc57634f
Luiz-1213 <146768585+Luiz-1213@users.noreply.github.com>
2025-04-03 17:43:32 -0300

    feat: create httpClient
    - Creates HttpClient class to manage HTTP requests
    - Adds support for dynamic headers, including JWT token
    - Implements GET, POST, PUT and DELETE methods with `_makeRequest`
    - Uses `flutter_secure_storage` to store and retrieve the authentication token

---
+++ app_money_flow/lib/src/core/services/http/http_client.dart
@@ -0,0 +1,55 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class HttpClient {
+  final String baseUrl;
+  final _storage = const FlutterSecureStorage();
+
+  HttpClient(this.baseUrl);
+
+  Future<Map<String, String>> _getHeaders() async {
+    String? token = await _storage.read(key: 'access_token');
+    return {
+      'Content-Type': 'application/json',
+      if (token != null) 'Authorization': 'Bearer $token',
+    };
+  }
+
+  Future<dynamic> get(String path) async {
+    return _makeRequest(path, 'GET');
+  }
+
+  Future<dynamic> post(String path, {dynamic body}) async {
+    return _makeRequest(path, 'POST', body: body);
+  }
+
+  Future<dynamic> put(String path, {dynamic body}) async {
+    return _makeRequest(path, 'PUT', body: body);
+  }
+
+  Future<dynamic> delete(String path) async {
+    return _makeRequest(path, 'DELETE');
+  }
+
+  Future<dynamic> _makeRequest(String path, String method, {dynamic body}) async {
+    final uri = Uri.parse('$baseUrl$path');
+    final headers = await _getHeaders();
+
+    final response = http.Request(method, uri)
+      ..headers.addAll(headers)
+      ..body = body ?? '';
+
+    final streamedResponse = await response.send();
+    final responseData = await http.Response.fromStream(streamedResponse);
+
+    return _handleResponse(responseData);
+  }
+
+ dynamic _handleResponse(http.Response response) {
+  if (response.statusCode >= 200 && response.statusCode < 300) {
+    return response.body; // Retorna o corpo da resposta como String
+  } else {
+    throw Exception('Erro na requisição: ${response.statusCode}');
+  }
+}
+}
